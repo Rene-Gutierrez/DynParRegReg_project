f17fa990d9ba837686a7080d389d59fd5683fa83
Rene Gutierrez Marquez <rgutie17@ucsc.edu>
2020-09-28 01:47:44 -0600

    updated runners

--- bayLasRun_jerez.R
+++ bayLasRun_jerez.R
@@ -19,7 +19,7 @@ XDatPat <- paste0("./dat/datX-", datTyp, "-")
 yDatPat <- paste0("./dat/daty-", datTyp, "-")
 
 # Set-Up
-ite    <- 1
+ite    <- 4
 per    <- get(load(file = paste0('./dat/per-', ite,'.RData')))
 coe    <- get(load(file = paste0('./dat/coe-',ite,'.RData')))
 gr1    <- get(load(file = paste0('./dat/gr1-',ite,'.RData')))
@@ -72,7 +72,7 @@ regMod    <- "bayLas"
 met       <- "dfp"
 
 # Set-Up
-ite    <- 1
+ite    <- 4
 per    <- get(load(file = paste0('./dat/per-', ite,'.RData')))
 coe    <- get(load(file = paste0('./dat/coe-',ite,'.RData')))
 gr1    <- get(load(file = paste0('./dat/gr1-',ite,'.RData')))
@@ -123,7 +123,165 @@ regMod    <- "bayLas"
 met       <- "dfp"
 
 # Set-Up
-ite    <- 1
+ite    <- 4
+per    <- get(load(file = paste0('./dat/per-', ite,'.RData')))
+coe    <- get(load(file = paste0('./dat/coe-',ite,'.RData')))
+gr1    <- get(load(file = paste0('./dat/gr1-',ite,'.RData')))
+rb     <- get(load(file = './dat/rb.RData'))
+M      <- 60
+# Initial Random Partition
+p      <- length(coe)
+P      <- list()
+rem    <- p %% M
+if(rem == 0){
+  comNum <- p %/% M 
+} else {
+  comNum <- p %/% M + 1
+}
+for(i in 1:(comNum - 1)){
+  P[[i]] = coe[((i - 1) * M + 1):(i * M)]
+}
+if(rem == 0){
+  P[[comNum]] <- coe[((comNum - 1) * M + 1):(comNum * M)]
+} else {
+  P[[comNum]] <- coe[((comNum - 1) * M + 1):((comNum - 1) * M + rem)]
+}
+
+# Creates the Paths
+curParPat <- paste0('./out/curPar-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+savPat    <- paste0('./out/savPar-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+staPat    <- paste0('./out/sta-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+
+out <- dfpBayLas(staBat    = 1,
+                 endBat    = 500,
+                 savCoe    = c(gr1, gr2, gr3),
+                 curParPat = curParPat,
+                 savPat    = savPat,
+                 staPat    = staPat,
+                 XDatPat   = XDatPat,
+                 yDatPat   = yDatPat,
+                 datPer    = per[ite],
+                 M         = M,
+                 iniPar    = P,
+                 staPar    = list(hb = rep(0, p),
+                                  ht = rep(1, p),
+                                  hs = 1,
+                                  hl = 1))
+
+# Problem Type
+datTyp    <- "low-high"
+regMod    <- "bayLas"
+met       <- "dfp"
+
+XDatPat <- paste0("./dat/datX-", datTyp, "-")
+yDatPat <- paste0("./dat/daty-", datTyp, "-")
+
+# Set-Up
+ite    <- 7
+per    <- get(load(file = paste0('./dat/per-', ite,'.RData')))
+coe    <- get(load(file = paste0('./dat/coe-',ite,'.RData')))
+gr1    <- get(load(file = paste0('./dat/gr1-',ite,'.RData')))
+gr2    <- get(load(file = paste0('./dat/gr2-',ite,'.RData')))
+gr3    <- get(load(file = paste0('./dat/gr3-',ite,'.RData')))
+rb     <- get(load(file = './dat/rb.RData'))
+M      <- 40
+# Initial Random Partition
+p      <- length(coe)
+P      <- list()
+rem    <- p %% M
+if(rem == 0){
+  comNum <- p %/% M 
+} else {
+  comNum <- p %/% M + 1
+}
+for(i in 1:(comNum - 1)){
+  P[[i]] = coe[((i - 1) * M + 1):(i * M)]
+}
+if(rem == 0){
+  P[[comNum]] <- coe[((comNum - 1) * M + 1):(comNum * M)]
+} else {
+  P[[comNum]] <- coe[((comNum - 1) * M + 1):((comNum - 1) * M + rem)]
+}
+
+# Creates the Paths
+curParPat <- paste0('./out/curPar-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+savPat    <- paste0('./out/savPar-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+staPat    <- paste0('./out/sta-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+
+out <- dfpBayLas(staBat    = 1,
+                 endBat    = 500,
+                 savCoe    = c(gr1, gr2, gr3),
+                 curParPat = curParPat,
+                 savPat    = savPat,
+                 staPat    = staPat,
+                 XDatPat   = XDatPat,
+                 yDatPat   = yDatPat,
+                 datPer    = per[ite],
+                 M         = M,
+                 iniPar    = P,
+                 staPar    = list(hb = rep(0, p),
+                                  ht = rep(1, p),
+                                  hs = 1,
+                                  hl = 1))
+
+# Problem Type
+datTyp    <- "low-High"
+regMod    <- "bayLas"
+met       <- "dfp"
+
+# Set-Up
+ite    <- 7
+per    <- get(load(file = paste0('./dat/per-', ite,'.RData')))
+coe    <- get(load(file = paste0('./dat/coe-',ite,'.RData')))
+gr1    <- get(load(file = paste0('./dat/gr1-',ite,'.RData')))
+rb     <- get(load(file = './dat/rb.RData'))
+M      <- 50
+# Initial Random Partition
+p      <- length(coe)
+P      <- list()
+rem    <- p %% M
+if(rem == 0){
+  comNum <- p %/% M 
+} else {
+  comNum <- p %/% M + 1
+}
+for(i in 1:(comNum - 1)){
+  P[[i]] = coe[((i - 1) * M + 1):(i * M)]
+}
+if(rem == 0){
+  P[[comNum]] <- coe[((comNum - 1) * M + 1):(comNum * M)]
+} else {
+  P[[comNum]] <- coe[((comNum - 1) * M + 1):((comNum - 1) * M + rem)]
+}
+
+# Creates the Paths
+curParPat <- paste0('./out/curPar-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+savPat    <- paste0('./out/savPar-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+staPat    <- paste0('./out/sta-', datTyp, "-", regMod, "-", met, "-", ite, "-", M, ".RData")
+
+out <- dfpBayLas(staBat    = 1,
+                 endBat    = 500,
+                 savCoe    = c(gr1, gr2, gr3),
+                 curParPat = curParPat,
+                 savPat    = savPat,
+                 staPat    = staPat,
+                 XDatPat   = XDatPat,
+                 yDatPat   = yDatPat,
+                 datPer    = per[ite],
+                 M         = M,
+                 iniPar    = P,
+                 staPar    = list(hb = rep(0, p),
+                                  ht = rep(1, p),
+                                  hs = 1,
+                                  hl = 1))
+
+# Problem Type
+datTyp    <- "low-High"
+regMod    <- "bayLas"
+met       <- "dfp"
+
+# Set-Up
+ite    <- 7
 per    <- get(load(file = paste0('./dat/per-', ite,'.RData')))
 coe    <- get(load(file = paste0('./dat/coe-',ite,'.RData')))
 gr1    <- get(load(file = paste0('./dat/gr1-',ite,'.RData')))
